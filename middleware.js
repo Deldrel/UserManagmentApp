894bf850fae0912c09f195c8a44b737a8c05f489
Deldrel <plapolla9@gmail.com>
2023-11-30 12:12:49 +0100

    feat: initial

---
+++ middleware.js
@@ -0,0 +1,22 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+export async function middleware(req) {
+    const res = NextResponse.next()
+    const supabase = createMiddlewareClient({ req, res })
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
+    // if user is signed in and the current path is / redirect the user to /account
+    if (user && req.nextUrl.pathname !== '/account') {
+        return NextResponse.redirect(new URL('/account', req.url))
+    }
+
+    return res
+}
+
+export const config = {
+    matcher: ['/', '/account'],
+}
